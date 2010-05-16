510f2a527c9b4abb9b64514cb47c778236803af9
Federico Ceratto <federico.ceratto@gmail.com>
2010-05-16 11:45:52 +0100

    Initial debianization.

---
+++ debian/easyzone.default.ex
@@ -0,0 +1,10 @@
+# Defaults for easyzone initscript
+# sourced by /etc/init.d/easyzone
+# installed at /etc/default/easyzone by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(easyzone):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="easyzone" command="/usr/bin/easyzone"
